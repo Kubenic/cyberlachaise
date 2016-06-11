9868d0e76539dd402c4f885187472b2a5a8b334d
Xening <rom.roullet@gmail.com>
2016-06-12 01:20:03 +0200

    rangement accueil
    rangement de l'archi de l'accueil

---
+++ accueil/js/app.js
@@ -0,0 +1,36 @@
+var bgVidContainer = document.querySelector('.video');
+bgVidContainer.style.height= window.innerHeight +'px';
+
+document.querySelector('.virgule video').addEventListener('ended',function(e){
+  document.querySelector('.virgule').style.display="none";
+},false);
+
+$('.fullpage').fullpage({
+    scrollingSpeed:300,
+      onLeave: function(index, nextIndex, direction){
+          var leavingSection = $(this);
+          var virgule, virguleVid;
+
+          virgule = document.querySelector('.virgule');
+          virguleVid = virgule.querySelector('video');
+
+          virgule.style.display='';
+          virguleVid.play();
+
+          //after leaving section 2
+          if(index == 2 && direction =='down'){
+             // alert("Going to section 3!");
+          }
+
+          else if(index == 2 && direction == 'up'){
+             // alert("Going to section 1!");
+              var bgVid = document.querySelector('.video video');
+          bgVid.play();
+          }
+      },
+      afterRender: function(){
+          var pluginContainer = $(this);
+          var bgVid = document.querySelector('.video video');
+          bgVid.play();
+      }
+  });
